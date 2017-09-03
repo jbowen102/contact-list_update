2fdcada1f787172168447d8cafb70c2b6c003c1f
Jonathan Bowen <jjbowen19@gmail.com>
2017-09-03 17:14:09 -0400

    Added GitHub URL to setup.py.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
     'description': 'Contact List Update',
     'author': 'Jonathan Bowen',
-    'url': 'N/A'
-    'download_url': 'N/A',
+    'url': 'https://github.com/jbowen102/contact-list_update'
+    'download_url': 'https://github.com/jbowen102/contact-list_update',
     'author_email': 'jjbowen19@gmail.com',
     'version': '0.1',
     'install_requires': ['nose', 'pandas'],
