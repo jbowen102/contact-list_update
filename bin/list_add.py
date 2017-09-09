3e21daab53985ac1aa48a193e125bb7d22d1f75a
Jonathan Bowen <jjbowen19@gmail.com>
2017-09-09 17:15:14 -0400

    Add block for checking if rows are same (list_add).

--- bin/list_add.py
+++ bin/list_add.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 import pandas as pd
 from list_write import date_time_str
 
@@ -20,10 +21,16 @@ def list_add(df_current, df_input):
         # Known issue: Editing a name will cause the new one to be added in
         # addition to old one, not replacing.
         if not ser_key in df_current.keys():
-
             df_out[ser_key] = df_input[ser_key]
             df_out[ser_key]['Mod Date'] = date_time_str('short')
             print('Added %s to output DF' % ser_key)
+
+        # If the two series are not exactly the same, use the new one.
+        elif not (md5(df_input[ser_key].values).hexdigest() ==
+                    md5(df_current[ser_key].values).hexdigest()):
+            df_out[ser_key] = df_input[ser_key]
+            df_out[ser_key]['Mod Date'] = date_time_str('short')
+            print('Modified %s' % ser_key)
 
     return df_out.sort_index(axis=1)
 
@@ -33,16 +40,14 @@ from list_read import list_read
 from list_write import list_write
 from list_sort import master_field_list
 # filename1 = './master/Sample_iPhone_Export_2.csv'
-# filename1 = './master/MyContacts-2017-08-10-210940-230_shortened.csv'
-filename1 = './master/2017-07-12_TSV_Contacts.csv'
-
+filename1 = './master/MyContacts-2017-08-10-210940-230_shortened.csv'
+# filename1 = './master/2017-07-12_TSV_Contacts.csv'
 df_current = list_read(filename1)
+
 # filename2 = './input_data/Sample_iPhone_Export_3.csv'
-# filename2 = './input_data/MyContacts-2017-08-10-210940-230.csv'
-filename2 = './input_data/2017-07-28_TSV_Contacts.csv'
-
+filename2 = './input_data/MyContacts-2017-08-10-210940-230.csv'
+# filename2 = './input_data/2017-07-28_TSV_Contacts.csv'
 df_input = list_read(filename2)
-df_out = list_add(df_current, df_input)
 
-# fields = master_field_list(filename1)
-list_write(df_out, desc='TSV_test_1')
+df_out = list_add(df_current, df_input)
+list_write(df_out, desc='iPhone_test_3')
