2cf225b55756ccb76166c6190ef501ec72484f5b
JB (TXT Comp) <jjbowen19@gmail.com>
2017-09-11 13:13:45 -0400

    Added Outlook mapping to master_field_dict function.

--- bin/list_sort.py
+++ bin/list_sort.py
@@ -59,40 +59,39 @@ def master_field_dict(master_file='./current_master/TB_default_fields.csv'
                     	'Birth Day': ''}
 
         if map_type == 'Outlook':
-            # map_dict = {'First Name': 'First Name',
-            #             'Last Name': 'Last Name',
-            #             'Display Name': '',
-            #             'Nickname': 'Nickname',
-            #             'Organization': 'Organization',
-            #             'Department': 'Department',
-            #             'Job Title': 'Title',
-            #             'Primary Email': 'Home [Email]',
-            #             'Secondary Email': 'Work [Email]',
-            #         	'Mobile Number': 'Mobile',
-            #         	'Home Phone': 'Home [Phone]',
-            #         	'Work Phone': 'Work [Phone]',
-            #         	'Fax Number': 'Work Fax',
-            #         	'Web Page 1': 'Home Page',
-            #         	'Home Address': 'Home [Address]',
-            #         	'Work Address': 'Work [Address]',
-            #         	'Notes': 'Note',
-            #         	'Pager Number': 'Pager',
-            #         	'Home Address 2': '',
-            #         	'Home City': '',
-            #         	'Home State': '',
-            #         	'Home ZipCode': '',
-            #         	'Screen Name': '',
-            #         	'Home Country': '',
-            #         	'Work Address 2': '',
-            #         	'Work City': '',
-            #         	'Work State': '',
-            #         	'Work ZipCode': '',
-            #         	'Work Country': '',
-            #         	'Web Page 2': '',
-            #         	'Birth Year': '',
-            #         	'Birth Month': '',
-            #         	'Birth Day': ''}
-
+            map_dict = {'First Name': 'First Name',
+                        'Last Name': 'Last Name',
+                        'Display Name': 'Title',
+                        'Nickname': '',
+                        'Organization': 'Company',
+                        'Department': 'Department',
+                        'Job Title': 'Job Title',
+                        'Primary Email': 'E-mail Address',
+                        'Secondary Email': 'E-mail 2 Address',
+                    	'Mobile Number': 'Mobile Phone',
+                    	'Home Phone': 'Home Phone',
+                    	'Work Phone': 'Business Phone',
+                    	'Fax Number': 'Business Fax',
+                    	'Web Page 1': 'Web Page',
+                    	'Home Address': 'Home Street',
+                    	'Work Address': 'Business Street',
+                    	'Notes': 'Notes',
+                    	'Pager Number': 'Pager',
+                    	'Home Address 2': 'Home Street 2',
+                    	'Home City': 'Home City',
+                    	'Home State': 'Home State',
+                    	'Home ZipCode': 'Home Postal Code',
+                    	'Screen Name': '',
+                    	'Home Country': 'Home Country/Region',
+                    	'Work Address 2': 'Business Street 2',
+                    	'Work City': 'Business City',
+                    	'Work State': 'Business State',
+                    	'Work ZipCode': 'Business Postal Code',
+                    	'Work Country': 'Home Country/Region',
+                    	'Web Page 2': '',
+                    	'Birth Year': '',
+                    	'Birth Month': '',
+                    	'Birth Day': ''}
 
 
     elif map_type == 'iPhone':
@@ -103,10 +102,6 @@ def master_field_dict(master_file='./current_master/TB_default_fields.csv'
 
     # Ensure Thunderbird headings match hard-coded dict keys
     assert set(field_list) == set(map_dict.keys())
-
-
-
-    for i in field_list:
 
 
 
