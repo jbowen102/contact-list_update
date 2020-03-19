b29d1dae11b25af2053588e40669d6122753ce81
Jonathan Bowen <ew15dro6k216@opayq.net>
2020-03-19 19:09:42 -0400

    Allow auto-select of lone file in input_data dir for both Combine and Reformat.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import time
 import csv
+from os import listdir
+from os.path import exists as path_exists
 from hashlib import md5
 import pandas as pd
 from bin import list_read as lr
@@ -8,18 +10,34 @@ from bin import field_reorder as fr
 from bin import list_write as lw
 
 
+class InputDirectoryError(Exception):
+    pass
+
+
 def combine_prog():
     print('*' * 10, 'Combine program', '*' * 10)
     current_filename = input("Enter the filepath of current master list to "
                 "add entries to:\n>>> ")
     df_current = lr.list_read(current_filename)
 
-    input_filename = input("\nEnter the filepath of new import list:\n>>> ")
-    df_input = lr.list_read(input_filename)
+    input_filename = input("\nEnter the filepath of new import list or press "
+        "Enter to use default\n (whatever lone file is in the 'input_files' "
+        "directory):\n>>> ")
+
+    if input_filename == "":
+        avail_input_files = listdir('./input_data/')
+        if len(avail_input_files) == 1:
+            df_input = lr.list_read('./input_data/' + avail_input_files[0])
+        else:
+            raise InputDirectoryError("There must be exactly one file in the 'input_data' dir.")
+    elif path_exists(input_filename):
+        df_input = lr.list_read(input_filename)
+    else:
+        raise InputDirectoryError("Invalid response. Start over.")
 
     df_out = lc.list_combine(df_current, df_input)
 
-    desc = input("Enter a description for output-file name (like iPhone or "
+    desc = input("Enter a description for output-file name (like 'iPhone' or "
                 "'Outlook') or press Enter for none:\n>>> ")
     new_file = lw.list_write(df_out, desc)
 
@@ -30,16 +48,26 @@ def reformat_prog():
                 " (TB import format) or press Enter to use default\n"
                 "\t('master/TB_default_fields.csv'):\n>>> ")
     reorder_filename = input("Enter the filepath of list to reorder "
-                "according to the TB field format:\n>>> ")
+                "according to the TB field format or press Enter to use default\n"
+                "(whatever lone file is in the 'input_files' directory):\n>>> ")
 
-    df_to_reorder = lr.list_read(reorder_filename)
+    if reorder_filename == "":
+        avail_input_files = listdir('./input_data/')
+        if len(avail_input_files) == 1:
+            df_to_reorder = lr.list_read('./input_data/' + avail_input_files[0])
+        else:
+            raise InputDirectoryError("There must be exactly one file in the 'input_data' dir.")
+    elif path_exists(reorder_filename):
+        df_to_reorder = lr.list_read(reorder_filename)
+    else:
+        raise InputDirectoryError("Invalid response. Start over.")
 
     if master_filename:
         df_out = fr.field_reorder(df_to_reorder, master_filename)
     else:
         df_out = fr.field_reorder(df_to_reorder)
 
-    desc = input("Enter a description for output-file name (like 'iPhone '"
+    desc = input("Enter a description for output-file name (like 'iPhone' "
                 "or 'Outlook') or press Enter for none:\n>>> ")
     new_file = lw.list_write(df_out, desc)
 
@@ -71,6 +99,7 @@ while True:
         "newly-added or modified entries \n"
         "\t\tfound in an input CSV file recently exported.\n"
         "\t-The formats supported are iPhone- and Outlook-generated CSV exports.\n"
+        "\t-Intent is to accept and combine input and master lists in SOURCE format, not TB format.\n"
         "\t-Before running, check input CSV for anomolies. Correct in input "
         "file or on source device (and re-export) \n"
         "\t\tbefore proceeding with program.\n"
@@ -78,9 +107,8 @@ while True:
         "\t-Ensure the most recent master list CSV is in the 'master' directory.\n"
         "\t-Program will ask for name of input and master lists (which do not "
         "need to be in the program directories).\n"
-        "\t-Intent is to accept and combine input and master lists in SOURCE format, not TB format.\n"
         "\t-Program will combine to make a new master list with the union of records "
-        "found in the two programs. \n"
+        "found in the two files. \n"
         "\t-Any new records found in input file will be added to master list, and "
         "any that have been modified \n"
         "\t\tsince last combine will replace the previous versions in the master list.\n"
